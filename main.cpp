1f09bd2062a7b8b1b2cc516ce6ba328c72337aea
Nineo81 <boris.belgique@hotmail.fr>
2018-10-22 17:40:50 +0200

    Ceci est un test

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     QPushButton bouton("Vous êtes mauvais !");
-    bouton.show();
+    bouton.show();/*Ceci est un test*/
 
     return app.exec();
 }
