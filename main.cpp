a58bdb136b9230ec1232af57f3b5ab32ae6fe9b9
Céline <kimloancelinetran@gmai.com>
2018-10-23 09:16:18 +0200

    Ceci est un test 3

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     QPushButton bouton("Vous êtes mauvais !");
-    bouton.show();
+    bouton.show();/*Ceci est un nouvea test*/
 
     return app.exec();
 }
