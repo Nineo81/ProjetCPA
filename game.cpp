5a3e9e63d4e28503968fc5c9df09d3a03b49bb98
Delphine <delsomer@hotmail.com>
2018-11-13 15:18:56 +0100

    condition pour la création du defenseChart simplifiée

--- game.cpp
+++ game.cpp
@@ -21,23 +21,17 @@ Game::Game():cursor(5,5),terrainMap("Map1V1.txt"),
     for (unsigned int i = 0; i<terrainMap.getSize('m');i++){
         defenseChart[i].resize(terrainMap.getSize('p'));
         for (unsigned int j = 0; j<terrainMap.getSize('p');j++){
-            int temp =terrainMap.getElement(i,j);
-            if (temp == 4 || temp == 5 ||temp == 6 ||temp == 7 ||temp == 8 ||temp == 9 ||
-                temp == 10 ||temp == 11 ||temp == 12 ||temp == 13 ||temp == 14 ||temp == 15 ||
-                temp == 16 ||temp == 17 ||temp == 18 ||temp == 19 ||temp == 20 ||temp == 21 ||
-                temp == 22 ||temp == 23 ||temp == 24 ||temp == 25 ||temp == 26 ||temp == 27 ||
-                temp == 28 ||temp == 29 ||temp == 30 ||temp == 31 ||temp == 32 ||temp == 101 ||
-                temp == 102 ||temp == 103 ||temp == 104 ||temp == 105 ||temp == 106 ||temp == 107 ||
-                temp == 108 ||temp == 109 ||temp == 110){
+            int n =terrainMap.getElement(i,j);
+            if ((n >= 4 && n <= 32)||n >= 101){
               defenseChart[i][j] = 0;
             }
-            else if (temp == 1 ||temp == 33){
+            else if (n == 1 ||n == 33){
               defenseChart[i][j] = 1;
             }
-            else if (temp == 3){
+            else if (n == 3){
               defenseChart[i][j] = 2;
             }
-            else if (temp == 2){
+            else if (n == 2){
               defenseChart[i][j] = 4;
             }
             else {defenseChart[i][j] = 3;}
