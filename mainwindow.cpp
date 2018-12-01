4735e5e9836a8e24c5641db013b178ca8cd5197d
Delphine <delsomer@hotmail.com>
2018-12-01 18:55:11 +0100

    Premier jet pour la communication réseau dans mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,6 +15,76 @@ MainWindow::MainWindow(Map *terrainMap,Map *unitMap,Cursor* cursor) : cursor(cur
     centerZone.setSize(screenGeometry.width()*0.9,screenGeometry.height()*0.9);
     this->adjustSize();
     move(screen->availableGeometry().center()-this->rect().center());
+
+    /*création du serveur? */
+    server = new QTcpServer();
+    if(! server->listen(QHostAddress::Any, 8123)) {
+        std::cout << "I am a client" << std::endl;
+        other = new QTcpSocket();
+        connect(other, SIGNAL(connected()), this, SLOT(onConnected()));
+        other->connectToHost("127.0.0.1", 8123);
+        connect(other, SIGNAL(disconnected()), this, SLOT(onDisconnected()));
+        }
+    else {
+        std::cout << "I am the server" << std::endl;
+        other = nullptr;
+        }
+    connect(server, SIGNAL(newConnection()), this, SLOT(onNewConnection()));
+}
+
+void MainWindow::onNewConnection() {
+    std::cout << "A new client is connecting !" << std::endl;
+    other = server->nextPendingConnection();
+    connect(other, SIGNAL(disconnected()), this, SLOT(onDisconnected()));
+    connect(other, SIGNAL(readyRead()), this, SLOT(onData()));
+    QJsonObject config;
+    config["income"] = 1000;
+    config["map"] = 25087;
+    config["firstplayer"] = 5;
+    config["firstplayer"] = 10;
+    config["youplay"] = "firstPlayer";
+    QJsonObject unit1;
+    unit1["country"] = 10;
+    unit1["type"] = "infantry";
+    unit1["x"] = 15;
+    unit1["y"] = 3;
+    config["units"] = unit1;
+    sendJson(config);
+}
+
+void MainWindow::onDisconnected() {
+    std::cout << "The other guy just disconnected" << std::endl;
+}
+
+void MainWindow::onConnected() {
+    std::cout << "I am connected" << std::endl;
+    connect(other, SIGNAL(readyRead()), this, SLOT(onData()));
+}
+
+void MainWindow::onData() {
+    std::cout << "Some data !" << std::endl;
+    if(currentSize == 0) {
+        if(other->bytesAvailable() < 4)
+            return;
+        QDataStream in(other);
+        in >> currentSize;
+    }
+    if(other->bytesAvailable() < currentSize)
+        return;
+    QByteArray data = other->read(currentSize);
+    std::cout << data.toStdString() << std::endl;
+    currentSize = 0;
+    //conversion en QJsonObject
+    QJsonDocument doc = QJsonDocument::fromJson(data);
+    QJsonObject json = doc.object();
+}
+
+void MainWindow::sendJson(QJsonObject obj) {
+    QByteArray data = QJsonDocument(obj).toJson();
+    QDataStream out(other);
+    out << (quint32) data.length();
+    other->write(data);
+    std::cout << "Sending " << data.toStdString() << std::endl;
 }
 
 void MainWindow::keyPressEvent(QKeyEvent * event){
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include "gamewindow.h"
 #include "cursor.h"
+#include <QtNetwork>
 
 class MainWindow : public QMainWindow
 {
@@ -14,9 +15,20 @@ public:
     GameWindow* getWidget();
     void updateWidget();
 
+public slots:
+    void onNewConnection();
+    void onConnected();
+    void onDisconnected();
+    void onData();
+
 private:
     Cursor* cursor;
     GameWindow centerZone;
+
+    QTcpServer* server = nullptr;
+    QTcpSocket* other = nullptr;
+    quint32 currentSize = 0;
+    void sendJson(QJsonObject obj);
 
 signals:
 
