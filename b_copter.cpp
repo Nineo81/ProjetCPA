80869719ddebfe6352bf098a3ae71bba95866095
Céline <kimloancelinetran@gmai.com>
2018-12-12 22:31:46 +0100

    Données pour les units (oups)

--- b_copter.cpp
+++ b_copter.cpp
@@ -4,5 +4,9 @@ B_Copter::B_Copter(vector<unsigned int> pos,int color, int round,Game *game):Air
 {
     this->attack_type=1;
     this->type=59;
+    this->absMP=6;
+    this->MP=this->absMP;
+    this->cost=9000;
+    setUnit();
 
 }
