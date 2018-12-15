2c87abf8dd44a0d725016a07969b78cb7982e8d6
Céline <kimloancelinetran@gmai.com>
2018-12-15 21:34:37 +0100

    rien

--- game.cpp
+++ game.cpp
@@ -110,6 +110,7 @@ Game::Game(int gameType):terrainMap("Map1V1.txt"),
     cursor.getPlayer()->set_money(3000,'a');
     //création de l'unité de base de OS
     Unit* firstUnit = new infantery({15,3},1,0,this);
+
     firstUnit->setCanPlay(true);
     listPlayer[0]->add_unit(firstUnit);
 
